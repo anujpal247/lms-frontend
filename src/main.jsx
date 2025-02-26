f4394478bc2ecd28f9323fc9fce17138d549cc71
Anuj Pal <palanuj247@gmail.com>
2025-02-26 18:05:34 +0530

    Stared adding basic redux setup with tollkit

--- src/main.jsx
+++ src/main.jsx
@@ -5,13 +5,17 @@ import "./index.css";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { Toaster } from "react-hot-toast";
+import { Provider } from "react-redux";
 
 // component import
 import App from "./App.jsx";
+import store from "./Redux/store.js";
 
 createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
-    <App />
-    <Toaster />
-  </BrowserRouter>
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+      <Toaster />
+    </BrowserRouter>
+  </Provider>
 );
