3b90cfc7c4f8fa42aae86be54e034170a53b88d4
Anuj Pal <palanuj247@gmail.com>
2025-03-07 20:32:06 +0530

    added signup login buttons to sidebar

--- src/Layouts/HomeLayout.jsx
+++ src/Layouts/HomeLayout.jsx
@@ -1,8 +1,26 @@
 import { FiMenu } from "react-icons/fi";
 import { AiFillCloseCircle } from "react-icons/ai";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import Footer from "../Components/Footer";
+import { useDispatch, useSelector } from "react-redux";
+
 function HomeLayout({ children }) {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  // for checking if user is logged in
+  const isLoggedIn = useSelector((state) => state?.auth?.isLoggedIn);
+
+  // for displaying the options acc to role
+  const role = useSelector((state) => state?.auth?.role);
+
+  async function handleLogout(e) {
+    e.preventDefault();
+
+    // const res = await dispatch(logout());  // comes from auth slice
+    navigate("/");
+  }
+
   function hideDrawer() {
     const element = document.getElementsByClassName("drawer-toggle");
     element[0].checked = false;
@@ -30,7 +48,7 @@ function HomeLayout({ children }) {
         </div>
         <div className="drawer-side w-0">
           <label htmlFor="my-drawer" className="drawer-overlay"></label>
-          <ul className="menu w-48 sm:w-80 p-4 bg-base-100 text-base-content relative">
+          <ul className="menu w-48 sm:w-80 p-4 bg-base-100 text-base-content relative h-[90vh]">
             <li className="w-fit absolute right-2 z-50">
               <button onClick={hideDrawer}>
                 <AiFillCloseCircle size={"24px"} />
@@ -39,6 +57,11 @@ function HomeLayout({ children }) {
             <li>
               <Link to="/">Home</Link>
             </li>
+            {isLoggedIn && role === "ADMIN" && (
+              <li>
+                <Link to="/admin-dashboard">Admin DashBoard</Link>
+              </li>
+            )}
             <li>
               <Link to="/courses">All Courses</Link>
             </li>
@@ -48,6 +71,30 @@ function HomeLayout({ children }) {
             <li>
               <Link to="/contact">Contact Us</Link>
             </li>
+            {!isLoggedIn && (
+              <li className="bottom-4 absolute w-[90%]">
+                <div className="w-full flex items-center justify-center gap-2">
+                  <button className="bg-pink-400 px-4 py-1 font-semibold rounded-md w-full">
+                    <Link to="/login">Login</Link>
+                  </button>
+                  <button className="bg-gray-400 px-4 py-1 font-semibold rounded-md w-full">
+                    <Link to="/signup">Signup</Link>
+                  </button>
+                </div>
+              </li>
+            )}
+            {isLoggedIn && (
+              <li className="bottom-4 absolute w-[90%]">
+                <div className="w-full flex items-center justify-center gap-2">
+                  <button className="bg-pink-400 px-4 py-1 font-semibold rounded-md w-full">
+                    <Link to="/user/profile">Profile</Link>
+                  </button>
+                  <button className="bg-gray-400 px-4 py-1 font-semibold rounded-md w-full">
+                    <Link onClick={handleLogout}>Logout</Link>
+                  </button>
+                </div>
+              </li>
+            )}
           </ul>
         </div>
       </div>
